eab67d8cef920427d49fa5a1b689f3243edb7a71
Robbie Lamb <robbie.lamb@gmail.com>
2010-09-02 14:32:49 -0600

    Adding some seeds approporiate for Sapphire

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+begin
+
+  print "Creating System Roles..."
+
+  user_role = SystemRole.create(:name => 'User', :description => 'Default user in the system',
+                                :actions => ["project$retrieve"])
+  user_role.move(:to => 1)
+
+  project_manager_role = SystemRole.create(:name => 'Project Manager', :description => 'Able to create projects',
+                                     :actions => ["project$retrieve", "project$update", "role$retrieve", "user$retrieve", "role$retrieve"])
+  project_manager_role.move(:to => 2)
+
+  sys_admin = SystemRole.first_or_new(:name => 'Administrator', :description => 'System role for Administrators',
+                                      :actions => SystemRole.available_permissions)
+  sys_admin.move(:to => 3)
+
+  print "Done\n"
+
+  print "Creating Users..."
+
+  User.create(:login => 'yogo', :email => "yogo@montana.edu", :first_name => "System", :last_name => "Administrator", :password => 'change me', :password_confirmation => 'change me', :system_role => sys_admin)
+  
+  print "Done\n"
+  
+  print "Creating Roles..."
+  pi = Role.create(:name => "Principal Investigator", :description => "Principal Investigators create projects to pursue research goals.", :actions => ((Role.available_permissions - Role.to_permissions) + ['role$retrieve'] - ['project$destroy']))
+  pi.move(:to => 1)
+  
+  print "Done\n"
+end
