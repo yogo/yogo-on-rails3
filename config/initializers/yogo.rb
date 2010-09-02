c2c8d1865a4237264988496ede415839576b4fe8
Robbie Lamb <robbie.lamb@gmail.com>
2010-09-02 15:13:25 -0600

    Removing unneeded requires

--- config/initializers/yogo.rb
+++ config/initializers/yogo.rb
@@ -5,11 +5,6 @@
 #   FileUtils.mkdir_p(File.join(Rails.root, Yogo::Setting['asset_directory']))
 # end
 
-require 'facet'
-require 'datamapper/property/raw'
-require 'yogo_data_form_builder'
-require 'yogo_form_builder'
-require 'yogo/chainable'
 require 'yogo/project_ext'
 
 # Load the Application Version
