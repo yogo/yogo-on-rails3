fde9b0805e11d095cdab550a7de391c9d183ed3d
Robbie Lamb <robbie.lamb@gmail.com>
2010-09-02 10:41:14 -0600

    Adding in some yogo controllers

---
+++ app/controllers/yogo/projects_controller.rb
@@ -0,0 +1,61 @@
+# Yogo Data Management Toolkit
+# Copyright (c) 2010 Montana State University
+#
+# License -> see license.txt
+#
+# FILE: projects_controller.rb
+# The projects controller provides all the CRUD functionality for the project
+# and additionally: upload of CSV files, an example project and rereflection
+# of the yogo repository.
+
+class Yogo::ProjectsController < Yogo::BaseController
+  belongs_to :role, :user, :optional => true
+
+  ##
+  # Show all the projects
+  #
+  # @example 
+  #   get /projects
+  #
+  # @return [Array] Retrives all project and passes them to the view
+  #
+  # @author Yogo Team
+  #
+  # @api public
+  def index
+    super do |format|
+      if collection.empty?
+        @no_search = true
+        @no_menu   = true 
+        format.html { render('no_projects') }
+      else
+        format.html 
+      end
+    end
+  end
+  
+  protected
+
+  def resource
+    @project ||= resource_class.get(params[:id])
+  end
+  
+  def collection
+    @projects ||= resource_class.paginate(:page => params[:page], :per_page => 5)
+  end
+  
+  def resource_class
+    Yogo::Project
+  end
+  
+  with_responder do
+    def resource_json(project)
+      hash = super(project)
+      hash[:data_collections] = project.data_collections.map do |c| 
+        controller.send(:yogo_project_collection_path, project, c)
+      end
+      hash
+    end
+  end
+  
+end
