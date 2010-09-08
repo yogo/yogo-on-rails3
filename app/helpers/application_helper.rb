9ffeb75186eec48175aa31a7cfadd4c12ad8e247
Robbie Lamb <robbie.lamb@gmail.com>
2010-09-08 10:33:19 -0600

    Adding the tooltip method back in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,32 @@ module ApplicationHelper
   def clear_break
     raw "<br clear='all' style='clear: both;'/>"
   end
+  
+  ##
+  # Helper for creating a tooltip
+  # 
+  # @example
+  #   Here is an example
+  # 
+  # @param [String] body
+  # @param [String] title
+  # @param [Integer] length
+  # 
+  # @return [HTML Fragment]
+  # 
+  # @author yogo
+  # @api public
+  def tooltip(body, title = nil, length = 10)
+    id = UUIDTools::UUID.random_create
+    if body.length > length
+      <<-TT
+      <div id='#{id}' class='tooltip' title='#{title || "Click to see full text."}'>#{body}</div>
+      <span class='tooltip-snippet' onClick="$('##{id}').dialog('open')">
+        #{body[0..length]}<span class='more'>&#8230; more</span>
+      </span>
+      TT
+    else
+      body
+    end
+  end
 end
